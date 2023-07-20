f56190ad184493a6db57eb84a71f2d2240d1e748
sokudon <tdty15@gmail.com>
2023-07-20 11:06:54 +0900

    apit

---
+++ api_neta/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace api_neta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(495, 200);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 200);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ぼだ取得";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(731, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "https://api.matsurihi.me/api/mltd/v2/events/290/rankings/eventPoint/logs/";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.comboBox1.Items.AddRange(new object[] {
+            "17:00",
+            "00:00",
+            "21:00",
+            "----",
+            "00:00",
+            "01:00",
+            "02:00",
+            "03:00",
+            "04:00",
+            "05:00",
+            "06:00",
+            "07:00",
+            "08:00",
+            "09:00",
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00",
+            "16:00",
+            "17:00",
+            "18:00",
+            "19:00",
+            "20:00",
+            "21:00",
+            "22:00",
+            "23:00"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "17:00";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.comboBox2.Items.AddRange(new object[] {
+            "100,2500,5000,10000,25000,50000 ",
+            "100,2500,5000,10000"});
+            this.comboBox2.Location = new System.Drawing.Point(179, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(284, 23);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "100,2500,5000,10000,25000,50000";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 317);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ api_neta/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace api_neta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var finaldata = "----";
+            string[] rank ;
+
+            string s = ",";
+            char c = s[0];
+            rank = comboBox2.Text.Split(c);
+
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < rank.Length; i++) { 
+
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            DateTime dt = DateTime.Now;
+
+            string url = textBox2.Text + rank[i];
+
+            string timeset = comboBox1.Text;
+            string text = "";
+
+          
+                try
+                {
+                    text = wc.DownloadString(url);
+                    //File.WriteAllText(@"tmp.js", text);
+                }
+                catch (WebException exc)
+                {
+                    wc.Dispose();
+                    MessageBox.Show(exc.Message);
+                    return;
+                }
+        
+            wc.Dispose();
+
+            var json = Codeplex.Data.DynamicJson.Parse(text);
+
+            var j = json[0];
+            int[] arr = j.data;
+            int length = arr.Length - 1;
+            double finaldatas = 0;
+
+                if (timeset != "----")
+                {
+                    for (var ii = length; ii > 0; ii--)
+                    {
+                        var data = j.data[ii].aggregatedAt;
+                        string pattern = "T"+timeset;
+                        Match m = Regex.Match(data, pattern);
+                        if (m.Success)
+                        {
+
+                            finaldata = j.data[ii].aggregatedAt;
+                            finaldatas = j.data[ii].score;
+                            break;
+                        }
+
+                    }
+
+                }
+                else {
+
+                    var ii = length;
+                        var data = j.data[ii].aggregatedAt;
+                            finaldata = j.data[ii].aggregatedAt;
+                            finaldatas = j.data[ii].score;
+                }
+
+                sb.Append(rank[i].ToString() + "位; " + finaldatas + "\r\n");
+                sb.Append(finaldata.Replace("T", " ").Replace("-", "/") + "\r\n");
+            }
+            textBox1.Text = sb.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           Properties.Settings.Default.url = textBox2.Text;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            textBox2.Text = Properties.Settings.Default.url;
+            comboBox2.Text=Properties.Settings.Default.rank ;
+             comboBox1.Text=Properties.Settings.Default.time ;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            Properties.Settings.Default.time = comboBox1.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.rank = comboBox2.Text;
+        }
+    }
+}
